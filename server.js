51578b4bcdeb13f3984a1e1a44427116a68b7fd4
Mcdostone <pronoyann@gmail.com>
2015-11-22 15:30:18 +0100

    Foundations of the website

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
+var ejs = require('ejs');
 
 var app = express();
-
-
 var PORT = 3143;
 
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req, res) {
-	res.send("Wallpapers albums");
-})
+	res.render('home');
+});
+
+app.post('/generate', function(req, res) {
+	res.redirect('/');
+});
 
 app.listen(PORT, function() {
 	console.log("server is running: http://localhost:" + PORT);
-})
+});
