efe00b03847022f8a6c85c37061fb6d8c6d7d86c
Yann Prono <yann.prono@telecomnancy.net>
2015-12-21 20:23:31 +0100

    Temporary fix the JS client-side

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ window.onload = function() {
     var box_width = document.getElementById('width');
     var box_height = document.getElementById('height');
 
+
     var input_width = document.getElementById('input-width');
     var input_height = document.getElementById('input-height');
 
@@ -12,28 +13,32 @@ window.onload = function() {
     var initResolution = function() {
         box_width.innerHTML = width();
         box_height.innerHTML = height();
-
-        input_width.value = width();
-        input_height.value = height();
+        if(input_width) {
+            input_width.value = width();
+            input_height.value = height();    
+        }
     }
 
 
     initResolution();
 
-	input.addEventListener('input', function() {
-    	var img = document.getElementsByClassName('cover');
-    	var width = "25%";
-    	
-    	if(input.value)
-			width = 100 / input.value + "%";
-    	
-    	for (var i = img.length - 1; i >= 0; i--) {
-    		img[i].style.width = width;
-    	};
-    	
-	});
 
-    console.log(w_screen +" X " + h_screen)
+    if(input) {
+        input.addEventListener('input', function() {
+            var img = document.getElementsByClassName('cover');
+            var width = "25%";
+            
+            if(input.value)
+                width = 100 / input.value + "%";
+            
+            for (var i = img.length - 1; i >= 0; i--) {
+                img[i].style.width = width;
+            };
+        });
+    }
+	
+
+    console.log(width() +" X " + height())
 }
 
 
